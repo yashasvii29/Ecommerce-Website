c5d5e386b2f33d66ce55a9b3ae4a392976e852cb
yashasvi <yashasviagrawal29@gmail.com>
2023-11-14 01:16:40 +0530

    add models

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const mongoose=require('mongoose');
+const seedDB=require('./seed');
 
+mongoose.connect('mongodb://127.0.0.1:27017/Shopping-app')
+.then(()=>{
+    console.log("DB Connected successfully")
+})
+.catch(()=>{
+    console.log("DB error");
+    console.log(err);
+})
 
 
 
