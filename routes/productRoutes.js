76165f7a395b47aed14c1bd69477068c3a7c8596
yashasvi <yashasviagrawal29@gmail.com>
2023-11-25 01:12:15 +0530

    add client side validation

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,48 +7,84 @@ const Product=require('../models/Product');
 // express provide mini instance (router) we will use router 
 const router =express.Router()// mini instance
 // 1st route=> to show all the products
+// har router m try catch ka use krenge to handle the error
 router.get('/products',async(req,res)=>{
-    //1... Database m se data show krne ke liye pehle uss model(collection) ke anadr se data (products array) find krenge then data ko index page pr bhejenge
+    try{
+        //1... Database m se data show krne ke liye pehle uss model(collection) ke anadr se data (products array) find krenge then data ko index page pr bhejenge
     // 2...Product ke model(collection ) ke andar se products find krenge(means database m se data find krenge) and find() mongoose ka method hai (means db ka crud method hai) and y methods promise return krte hai ....promise ki chaining se bachne ke liye we will use async await
     let products=await Product.find({});
     // find method jo bhi return krega use ek products variable m store kr lenge and uss variable ko render method m object ke andar pass kr denge
     res.render('products/index',{products});
     // index ki file products folder ke andar hai that's why products/index
     // render method m {} ke andar products variable pass kiya hai which means index.ejs file ke andar products data bhej rhe hai......jab user /products page pr req send krega toh response m index page show hoga and index page pr products(means y data bhej rhe hai)
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
+    
 })
 
 // 2nd route=> to show the form for new product
 router.get('/product/new',(req,res)=>{
-    res.render('products/new')
+    try{
+        res.render('products/new')
     // response m new form show hoga
+    }
+    
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
 })
 //3rd route=> to add the new product to database and then redirect to the /products page
  //add product button pr click krte hi post request jayegi /products pr jo humne form define kiya hai in action attribute
 router.post('/products', async (req,res)=>{
-    // jab form submit hoga toh sara data req ki body m milega ....toh unn sabhi data ko object ke andar destructure krenge....(object ke andar vahi name likhte h jo humne schema m define kiya h)
-    let {name,img,price,desc}=req.body;  // body object ke data ko dekhne ke liye we will use the middleware app.use(express.urlencoded)
-    // database ke andar new product ko add krenge...means Product model ke andar new product create krenge
-    await Product.create({name,img,price,desc})//  create mongodb ka method hai and y promise return krta hai to promise ki chaining se bachne ke liye we will use async and await
-    // database ke andar new product add hone ke baad /products page pr redirect krenge
-    res.redirect('/products')// redirect means get req jayegi /products pr and sabhi products show ho jayenge with new product
-
+    try{
+            // jab form submit hoga toh sara data req ki body m milega ....toh unn sabhi data ko object ke andar destructure krenge....(object ke andar vahi name likhte h jo humne schema m define kiya h)
+            let {name,img,price,desc}=req.body;  // body object ke data ko dekhne ke liye we will use the middleware app.use(express.urlencoded)
+            // database ke andar new product ko add krenge...means Product model ke andar new product create krenge
+            await Product.create({name,img,price,desc})//  create mongodb ka method hai and y promise return krta hai to promise ki chaining se bachne ke liye we will use async and await
+            // database ke andar new product add hone ke baad /products page pr redirect krenge
+            res.redirect('/products')// redirect means get req jayegi /products pr and sabhi products show ho jayenge with new product
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
 })
 // pehle se likha hua code show ho rha hai toh enter tab
 // 4th route=> to show a particular product
 router.get('/products/:id',async(req,res)=>{
-    // id params object se milegi toh id ko object ke andar destructure krenge
+    try{
+        // id params object se milegi toh id ko object ke andar destructure krenge
     let {id} =req.params; // isse id mil jayegi
     // product model ke andar se product find krenge with the help of id
     let foundProduct= await Product.findById(id).populate('reviews'); //   product find krne ke baad use populate krenge(show krenge) reviews array ke sath means product ko show krenge reviews ke sath
     res.render('products/show',{foundProduct});// res m show page show hoga and uss show page wo product(foundProduct) bhej rhe hai
+
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
+
 })
-// if kisi chij ko show krna hai then we'll always send get request
+// if kisi chij ko show krna hai then we'll always send get request}
+    
 
 // 5th route=> show the form to edit the product(particular product)
 router.get('/products/:id/edit',async(req,res)=>{
-    let {id}=req.params;
-    let foundProduct= await Product.findById(id);
-    res.render('products/edit',{foundProduct});
+    try{
+        let {id}=req.params;
+        let foundProduct= await Product.findById(id);
+        res.render('products/edit',{foundProduct});
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
+   
 })
 // product m edit kr rhe hai(form ke andar edit kr he hai) means database ke andar changes kr rhe hai so we'll send patch req because edit means partial change (partial change ke liye we'll send patch req)
 // but form ke andar patch req nhi hoti ...form can send only get or post req
@@ -58,20 +94,32 @@ router.get('/products/:id/edit',async(req,res)=>{
 // 6th route=> to edit the product in database(means Product model m)
 // jab edit product button pr click krenge toh patch req jayegi iss url pr  /products/:id/edit
 router.patch('/products/:id',async(req,res)=>{
-    let {id} =req.params;
+    try{
+        let {id} =req.params;
     let {name,img,price,desc}=req.body;
     await Product.findByIdAndUpdate(id,{name,img,price,desc});// Product model(database) ke andar se uss product ko find krenge jise edit kiya hai ById and update kr denge
     res.redirect(`/products/${id}`);// id ko evaluate krne ke liye backtick ka use kiya h ...product ko edit krne ke baad (means edit product button pr click krte hi usi particular product pr redirect kr jayenge jise edit kiya h isliye redirect method m uss product ki id di h)
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
 
 })
 // 7th route=> to delete a particular product(delete krne ke liye post req ko delete req m override krenge means method override krenge and req send krne ke liye hume form ki need hoti hai so we will make delete form in the indes.ejs file)
 // database ke andar se product delete krna hai means post req jayegi but hum method override krenge...post req ko delete req m override kr denge and we will send delete request and product delete krne ke baad redirect krenge
 
 router.delete('/products/:id',async(req,res)=>{
-    let {id} =req.params;
-    const product = await Product.findById(id);
-    await Product.findByIdAndDelete(id);// findByIdAndDelete jab mongodb ka y method run hoga toh iss method ke behind the scene ek middleware chalega(means mongodb ka y method behind the scene middleware chala rha hai) jo Product ke schema pr apply kiya hai in Product.js file(because product ka schema model ke andar hota hai)
-    res.redirect('/products');
+    try{
+        let {id} =req.params;
+        const product = await Product.findById(id);
+        await Product.findByIdAndDelete(id);// findByIdAndDelete jab mongodb ka y method run hoga toh iss method ke behind the scene ek middleware chalega(means mongodb ka y method behind the scene middleware chala rha hai) jo Product ke schema pr apply kiya hai in Product.js file(because product ka schema model ke andar hota hai)
+        res.redirect('/products');
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
 })
 
 module.exports=router;// router ko export kr rhe hai toh app.js file ke andar require krenge
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -9,28 +9,33 @@ const Review = require('../models/Review');
 // we cannott write app.get and app.post here
 // express provide mini instance (router) we will use router 
 const router =express.Router()// mini instance
-
+// har router m try catch ka use krenge to handle the error
 router.post('/products/:id/review',async(req,res)=>{
     // console.log(req.body);
-    let {id}=req.params;
-    let {rating,comment}=req.body;
-    const product= await Product.findById(id);// product find krenge with the help of id(database ke andar se product find krenge with the help of id jise review dena hai)
-    // console.log(product);
-    // new review banayenge means Review model ka object banayenge(because model is a js class)
-    const review = new Review({rating,comment});
-    // res.send('review route');
-    // humne id ki help se jo product find kiya hai uss product ki reviews array m new review ko add kr denge(reviews array m new review ko push kr denge)
-    product.reviews.push(review);
-    // jis product ke andar reviews daale hai uss product ko save krenge and new review ko bhi save krenge
-    await review.save();// save mongodb ka method hai it returns a promise and promise ki chaining se bachne ke liye we will use async and await
-    await product.save();// save method is used to save (add) the document to the database
-    res.redirect(`/products/${id}`);
+    try{
+        let {id}=req.params;
+        let {rating,comment}=req.body;
+        const product= await Product.findById(id);// product find krenge with the help of id(database ke andar se product find krenge with the help of id jise review dena hai)
+        // console.log(product);
+        // new review banayenge means Review model ka object banayenge(because model is a js class)
+        const review = new Review({rating,comment});
+        // res.send('review route');
+        // humne id ki help se jo product find kiya hai uss product ki reviews array m new review ko add kr denge(reviews array m new review ko push kr denge)
+        product.reviews.push(review);
+        // jis product ke andar reviews daale hai uss product ko save krenge and new review ko bhi save krenge
+        await review.save();// save mongodb ka method hai it returns a promise and promise ki chaining se bachne ke liye we will use async and await
+        await product.save();// save method is used to save (add) the document to the database
+        res.redirect(`/products/${id}`);
     // res.send('reviews stored successfully');
     // hum chahte hai page pr redirect krne ke baad uss page pr reviews show ho jo humne uss product ko diye hai...... Product ke model(database) se reviews show krne hai means dusri collection se render krna hai so we will use populate(populate means show krna)...reviews array ke andar jo objectid hai usse data populate krenge
     // hum chahte hai jab hum ek particular product ko dekhe toh uss product ke corresponding(side se) sare reviews(uss particular product ke sabhi reviews) show ho jaye(populate ho jaye)..means jab ek particular product ko show krenge toh uss product ke sath sabhi reviews bhi show kr denge(it is known as populate) toh productRoutes.js file m show product ka jo route likha tha usme Product.findById().populate('reviews') populate method add kr denge and parenthesis m reviews array ka name pass kiya hai
     // jab ek collection se dusre collection ka data chahiye so we will use populate(means hum reviews ke collection pr kaam kr rhe hai but hume products ke collection se data chahiye in that case we will use populate)
     // jo review humne diya hai use dekh sakte hai in mongosh (run the command=> db.reviews.find({})) and y dekhne ke liye ki konse product ko review diya hai then run the command in mongosh(db.products.find({}))
-    
+    }
+    catch(e){
+        // e object m error hota hai toh err ko catch krenge and e object m message field bhi hota hai toh error ke message ko bhi bhejenge
+        res.status(500).render('error',{err:e.message});
+    }
 })
 
 module.exports=router;// router ko export kr rhe hai toh app.js file ke andar require krenge
